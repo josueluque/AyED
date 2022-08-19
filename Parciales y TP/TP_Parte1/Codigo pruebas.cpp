a48a16dc723c234e2d82965bb8e10f08d970170e
Josué Luque <89478802+JosueLuque@users.noreply.github.com>
2022-08-19 12:50:23 -0300

    Actualizaciones en TP 1
    - New carpeta de pruebas
    - Cambios en el archivo principal

--- Parciales y TP/TP_Parte1/Codigo pruebas.cpp
+++ Parciales y TP/TP_Parte1/Codigo pruebas.cpp
@@ -40,32 +40,18 @@ struct Curso
 void ingresarCursos(Curso vc[], int t, bool matCursos[][COL]);
 void inicializar(bool matCurso[][COL], int cf, int cc);
 void mostrarPorFila(bool matCursos[][COL], int cf, int cc);
+void punto1(Curso vc[], int t, bool matCursos[][COL], int cf, int cc);
 int ingDatoLimitado(int desde, int hasta);
-void sumatoriaColum(bool matCursos[][COL], int cf, int cc, int vc[]);
-void punto2(int vc[], int t);
-void punto3(int vc[], int t);
-
-void sumatoriaFilas(bool matCursos[][COL], int cf, int cc, int vf[]);
-void punto4(int vf[], int t);
 
 int main()
 {
     Curso vc[48];
     bool matCursos[FIL][COL];
-    int totalCursoIdioma[COL], totalCursoNivel[FIL];
+
     inicializar(matCursos, FIL, COL);
     ingresarCursos(vc, 48, matCursos);
     mostrarPorFila(matCursos, FIL, COL);
-    sumatoriaColum(matCursos, FIL, COL, totalCursoIdioma);
-    cout << "2) Lista de cursos que se dictarán por idioma: " << endl;
-    punto2(totalCursoIdioma, COL);
-
-    cout << "3) Cursos en los que se imparten los 8 niveles: " << endl;
-    punto3(totalCursoIdioma, COL);
-
-    sumatoriaFilas(matCursos, FIL, COL, totalCursoNivel);
-    cout << "4) Niveles en los que no se dictan clases en los 6 idiomas: " << endl;
-    punto4(totalCursoNivel, FIL);
+    // punto1(vc, 48, matCursos, FIL, COL);
 
     return 0;
 }
@@ -102,77 +88,6 @@ void ingresarCursos(Curso vc[], int t, bool matCursos[][COL])
     }
 }
 
-//  2) Informar por cada idioma la cantidad de cursos que se dictarán.
-
-void punto2(int vc[], int t)
-{
-    string vAux[6] = {"Inglés", "Frances", "Portugues", "Italiano", "Aleman", "Chino Mandarin"};
-    for (int i = 0; i < t; i++)
-    {
-        cout << "   " << vAux[i] << ": " << vc[i] << endl;
-    }
-}
-
-//  3) Informar, si hay, los idiomas que tienen cursos en los ocho niveles.
-
-void punto3(int vc[], int t)
-{
-    string vAux[6] = {"Inglés", "Frances", "Portugues", "Italiano", "Aleman", "Chino Mandarin"};
-    int cont = 0;
-    for (int i = 0; i < t; i++)
-    {
-        if (vc[i] == 8)
-        {
-            cout << "   - " + vAux[i] << endl;
-        }
-        else
-            cont++;
-    }
-    if (cont == t)
-    {
-        cout << "   - En ninguno de los idiomas se imparten los ocho niveles" << endl;
-    }
-}
-
-//  4) Informar, si hay, los niveles en los que no se dictará ningún idioma.
-void punto4(int vf[], int t)
-{
-    int cont = 0;
-    for (int i = 0; i < t; i++)
-    {
-        if (vf[i] == 0)
-        {
-            cout << "   Nivel " << i + 1 << endl;
-        }
-        else
-            cont++;
-    }
-    if (cont == t)
-    {
-        cout << "   - No existen los niveles en donde no se dicte ningún idioma" << endl;
-    }
-}
-
-void sumatoriaFilas(bool matCursos[][COL], int cf, int cc, int vf[])
-{
-    for (int i = 0; i < cf; i++)
-    {
-        vf[i] = 0;
-        for (int j = 0; j < cc; j++)
-            vf[i] += matCursos[i][j];
-    }
-}
-
-void sumatoriaColum(bool matCursos[][COL], int cf, int cc, int vc[])
-{
-    for (int j = 0; j < cc; j++)
-    {
-        vc[j] = 0;
-        for (int i = 0; i < cf; i++)
-            vc[j] += matCursos[i][j];
-    }
-}
-
 int ingDatoLimitado(int desde, int hasta)
 {
     int dato;
@@ -202,3 +117,57 @@ void inicializar(bool matCurso[][COL], int cf, int cc)
         for (int j = 0; j < cf; j++)
             matCurso[i][j] = 0;
 }
+
+/*
+    1) Generar los archivos “Ingles.dat”, “Frances.dat”, “Protugues.dat”, “Italiano.dat”,
+    “Aleman.dat” y “Chino.dat” con los datos de los cursos que se dictarán de cada uno de los
+    idiomas.
+    Cada registro debe tener el siguiente diseño:
+        Código del curso
+        Nivel
+        Cupo
+        Nombre y número de documento del docente a cargo
+    Estos archivos deben estar ordenados por código de curso.
+*/
+
+/*
+void punto1(Curso vc[], int t, bool matCursos[][COL], int cf, int cc)
+{
+    // Genero Archivos de los idiomas
+    FILE *archCursos;
+
+    string vAux[6] = {"Ingles", "Frances", "Portugues", "Italiano", "Aleman", "Chino Mandarin"};
+
+    // CUIDADO CON ESTA PARTE XD
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < cf; j++)
+        {
+            char nomArch[20] = "josue.dat";
+
+            archCursos = fopen(nomArch, "wb");
+            if (matCursos[j][i] == 1)
+            {
+
+                if (archCursos == NULL)
+                {
+                    cout << "ERROR" << endl;
+                }
+                else
+                {
+                    if (vc[j].nivel == j)
+                    {
+                        cin >> vc[j].codigo;
+                        cin >> vc[j].nivel;
+                        cin >> vc[j].cupo;
+                        cin >> vc[j].nombreDocente;
+                        cin >> vc[j].dniDocente;
+                        fwrite(&vc, sizeof(Curso), 1, archCursos);
+                    }
+                }
+            }
+        }
+        fclose(archCursos);
+    }
+}
+*/
